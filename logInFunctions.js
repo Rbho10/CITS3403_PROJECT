32521fe1f33fb2f80014538bfddabd5621448484
ToriCodie <ysabelle.villanueva20@gmail.com>
2025-04-14 12:13:00 +0800

    added frameworks/ideas of necessary functions to the log in page

--- logInFunctions.js
+++ logInFunctions.js
@@ -1,3 +1,11 @@
 /*
 Created javascript file
 */
+
+function validAlert(){
+    /*
+    when user tries to log in and both/either username or password does not align with those in database
+    throw alert that both/either username and/or password are incorrect
+    */
+    alert('Either username or password are incorrect.');
+}
