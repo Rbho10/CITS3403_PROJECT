e1e76737c9a75485359ba2bbe3e2674750882295
ToriCodie <ysabelle.villanueva20@gmail.com>
2025-04-29 00:20:51 +0800

    feat: added a form.py for creating a study subject

---
+++ app/createStudySubject_form.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, SelectField
+from wtforms.validators import InputRequired
+
+class CreateStudySubjectForm(FlaskForm):
+    subject_name = StringField("Subject Name", validators=[InputRequired()])
+    graph_type = SelectField("Graph Type", choices=[("line_graph", "Line Graph"),
+                                                    ("pie_graph", "Pie Graph"),
+                                                    ("bar_graph", "Bar Graph")])
+    graph_scale = SelectField("Graph Scale", choices=[(1, "One Month"),
+                                                      (3, "Three Months"),
+                                                      (6, "Six Months"),
+                                                      (12, "One Year"),
+                                                      (36, "Three Years")]) #integers are indicative of month length
+    privacy = SelectField("Privacy Setting", choices=[("public", "Public"), 
+                                                      ("private", "Private")
+                                                      ("hidden", "Hidden")])
+    opinion_toggle = BooleanField("Allow Opinions", default="checked")
+    submit = SubmitField('Create Subject')
